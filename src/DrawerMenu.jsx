114d2edd75c2b80dc2359ed404d2f6a954b4942f
Your Name <you@example.com>
2025-12-01 23:10:04 -0500

    Spelling error changes

--- src/DrawerMenu.jsx
+++ src/DrawerMenu.jsx
@@ -17,14 +17,13 @@ function DrawerMenu({ drawerOpen, toggleDrawer }) {
                     <Typography className="menu-header" variant="h6">About This Project</Typography>
                     <br></br>
                     <div>
-                        <p>This React app is built as a demonstartion to showcase my conceptual understanding of API fetching and data management.
+                        <p>This React app is built as a demonstration to showcase my conceptual understanding of API fetching and data management.
                             <br></br>
                             <br></br>
                             Utilizing MUI component and Framer Motion animation libraries.
                             <br></br>
                             <br></br>
-
-                            Thank you for youre feedback.
+                            Thank you for your feedback.
                         </p>
 
                     </div>
